e1daa28f284c692ae9d01b5dd07eef71803c877d
GHMusicalCoder <github@musicalcoder.com>
2018-10-02 16:55:16 -0400

    started working towards renaming

--- eBookRename.py
+++ eBookRename.py
@@ -29,6 +29,17 @@ def process_folder(path):
         print("----------")
 
 
+def new_process(path):
+    for folder in pathlib.Path(path).iterdir():
+        name = str(folder.parts[-1])
+        print("Folder Name >>> " + name)
+        for file in folder.iterdir():
+            if file.suffix == ".zip":
+                print("this one is zipped: " + file.name)
+            print("Rename attempt will be >>> " + file.stem + file.suffix + " >>> " + name + file.suffix)
+        print("*******************")
+
+
 def main():
     operating_system = platform()
     if operating_system[:5] == "Windo":
@@ -37,7 +48,8 @@ def main():
         main_path = get_linux_path()
 
     os.chdir(main_path)
-    process_folder(main_path)
+    # process_folder(main_path)
+    new_process(main_path)
 
 
 if __name__ == '__main__':
